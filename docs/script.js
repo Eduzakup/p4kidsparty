030dfccde5a5165c78219cf6eaeffce96ddb2f69
Eduzakup <sklep@eduzakup.pl>
2025-03-24 21:43:58 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("P4KidsParty aktywne!");
+// FAQ: zamykanie innych sekcji po otwarciu jednej
+document.addEventListener("DOMContentLoaded", function () {
+  const allDetails = document.querySelectorAll("details");
+
+  allDetails.forEach((detail) => {
+    detail.addEventListener("toggle", () => {
+      if (detail.open) {
+        allDetails.forEach((el) => {
+          if (el !== detail) el.removeAttribute("open");
+        });
+      }
+    });
+  });
 });
